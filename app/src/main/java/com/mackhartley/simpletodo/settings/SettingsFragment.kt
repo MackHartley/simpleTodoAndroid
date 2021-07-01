9d4c197842c7b1d0f1f06565796d8369a58eef01
Mack H <hartleymack@gmail.com>
2021-06-30 19:09:03 -0700

    Added privacy policy and TC

--- app/src/main/java/com/mackhartley/simpletodo/settings/SettingsFragment.kt
+++ app/src/main/java/com/mackhartley/simpletodo/settings/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.mackhartley.simpletodo.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +12,7 @@ import androidx.navigation.fragment.findNavController
 import com.auth0.android.authentication.AuthenticationException
 import com.auth0.android.callback.Callback
 import com.auth0.android.provider.WebAuthProvider
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mackhartley.simpletodo.R
 import com.mackhartley.simpletodo.common.ext.showError
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -27,6 +30,30 @@ class SettingsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
         val logOutBtn = view.findViewById<Button>(R.id.log_out_btn)
         logOutBtn.setOnClickListener { logout() }
+
+        val about = view.findViewById<View>(R.id.about)
+        val privacy = view.findViewById<View>(R.id.privacy)
+        val terms = view.findViewById<View>(R.id.terms)
+
+        about.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(getString(R.string.about))
+                .setMessage(getString(R.string.about_txt))
+                .setNegativeButton(getString(R.string.close)) { _, _ -> }
+                .setPositiveButton(getString(R.string.visit_github)) { _, _ ->
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/MackHartley/simpleTodoAndroid"))
+                    startActivity(intent)
+                }
+                .show()
+        }
+        privacy.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://sites.google.com/view/simpletodo-privacypolicy/home"))
+            startActivity(intent)
+        }
+        terms.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://sites.google.com/view/simpletodotc/home"))
+            startActivity(intent)
+        }
 
         return view
     }
